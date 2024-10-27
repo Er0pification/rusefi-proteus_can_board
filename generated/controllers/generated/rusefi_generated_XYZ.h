ed95cf8c6f3cb95bda60a942e28422a96e610775
GitHub git update Action <action@github.com>
2024-10-27 09:44:14 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_XYZ.h
+++ generated/controllers/generated/rusefi_generated_XYZ.h
@@ -1294,7 +1294,7 @@
 #define SentEtbType_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 298999567
+#define SIGNATURE_HASH 2303089172
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1601,7 +1601,7 @@
 #define ts_show_acr_pins true
 #define ts_show_air_conditioning true
 #define ts_show_alternator true
-#define ts_show_analog_divider true
+#define ts_show_analog_divider false
 #define ts_show_analog_ego true
 #define ts_show_analog_input_settings true
 #define ts_show_aux_connections true
@@ -1640,7 +1640,8 @@
 #define ts_show_cylinder_bank true
 #define ts_show_debug_mode false
 #define ts_show_ego2 true
-#define ts_show_egt true
+#define ts_show_egt false
+#define ts_show_engine_control false
 #define ts_show_etb true
 #define ts_show_etb_bias_curve true
 #define ts_show_etb_extra_monitoring 
@@ -1755,7 +1756,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.10.27.XYZ.298999567"
+#define TS_SIGNATURE "rusEFI main.2024.10.27.XYZ.2303089172"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
